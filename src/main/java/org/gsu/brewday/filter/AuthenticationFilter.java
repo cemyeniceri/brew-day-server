5a2e575cbe7fdded09f3fdd66efd4dbcc1522d60
cyeniceri <cem.yeniceri@gmail.com>
2017-12-02 12:36:31 +0300

    Authentication filter is enabled for principals endpoints except login

--- src/main/java/org/gsu/brewday/filter/AuthenticationFilter.java
+++ src/main/java/org/gsu/brewday/filter/AuthenticationFilter.java
@@ -33,7 +33,7 @@ public class AuthenticationFilter extends GenericFilterBean {
         }
 
         final String path = ((HttpServletRequest) req).getRequestURI();
-        if (path.startsWith("/principals")) {
+        if (path.startsWith("/principals/login")) {
             LOG.info("Jwt filter disabled for login request");
             chain.doFilter(req, res);
             return;
